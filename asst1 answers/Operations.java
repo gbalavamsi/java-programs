20132a1cc3d2e9ec21de26c89aa2f6b736b2f8d9
gbalavamsi <131159616+gbalavamsi@users.noreply.github.com>
2023-07-12 08:45:20 +0530

    Add files via upload

---
+++ asst1 answers/Operations.java
@@ -0,0 +1,48 @@
+/*  JFM1T2_Assignment1:
+
+     Write a program to perform the below actions and print the result.
+     1. Difference of 2 numbers (30 and 10)
+     2. Product of 2 numbers (45 and 2)
+     3. Division of 2 numbers (600 and 10)
+     4. Increment and Decrement the number (15)
+     5. Remainder of 2 numbers (14 and 5)
+     Prompt the user input from the terminal.
+*/
+
+//import statements for java program to read inputs using Scanner class
+import java.util.Scanner;
+
+public class Operations {
+  public static void main(String [] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter a number");
+    int a=sc.nextInt();
+    System.out.println("Enter b number");
+    int b=sc.nextInt();
+    System.out.println("difference"+(a-b));
+    System.out.println("product"+(a*b));
+    System.out.println("Division"+(a/b));
+    System.out.println("increment"+(++a));
+    System.out.println("decrement"+(--b));
+    System.out.println("remainder"+(a%b));
+  }
+
+//main method
+
+/*
+  Use the scanner class to provide input at execution time using scanner object
+  Scanner sc=new Scanner(System.in);
+*/
+
+/*
+  Take input from user
+  System.out.println("Enter first number: ");
+  int a=sc.nextInt();
+*/
+
+//calculate difference,product,division,increment and remainder 
+
+//print appropriate result for all operations
+
+}
